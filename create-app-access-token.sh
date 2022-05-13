d42cddaa724ab4c8050df286f429a37be9ae2dc3
zneix <zneix@zneix.eu>
2022-05-13 18:55:02 +0200

    Added script for generating app access tokens

---
+++ create-app-access-token.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ -z $2 ]; then
+	echo "Usage: $(basename $0) <Client ID> <Client secret>"
+	exit 1
+fi
+
+curl -X POST https://id.twitch.tv/oauth2/token -d 'grant_type=client_credentials' -d "client_id=$1" -d "client_secret=$2" | jq
